70e080f75dd9535885012cb2998723eee1ebaa85
Bradley Williams <allblk13@gmail.com>
2025-04-02 22:24:25 -0500

    working on button fuctionality

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
 
-    $tweet.text(text);
+  const $tweets = streams.home.map((tweet) => {
+    const $tweet = $('<p></p>');
+    // username needs it's own tag
+    const twid = `${tweet.message}`
+    const user = `@${tweet.user}`;
+    //  needs to change to clickable element
+    $tweet.text(`${user} screams out into the endless void... ${twid}`);
 
     return $tweet;
   });
+    // create a function that creates new tweets
+  // Create a button 'Listen for new transmissions'
+  const $msgBtn = document.createElement('button');
+  $body.append($msgBtn)
+  $msgBtn.innerText = "Listen for new transmission"
+  $("button").click(function(){
+    
+  })
+
   $body.append($tweets);
 
 });
