6b41df73c770c688c4ad013520a9ad6e4a9bccff
Bradley Williams <allblk13@gmail.com>
2025-04-08 18:07:46 -0500

    trying things with username click

--- index.js
+++ index.js
@@ -1,37 +1,51 @@
-
 $(document).ready(() => {
   const $body = $('body');
-  const $div = $('<div>') // creates tag
-  
-  $body.html(''); // clears body
-  $body.prepend($div)
+  const $tweetHolder = $('<div>'); // Create a container for tweets
+  const $msgBtn = $('<button>').text('Listen for incoming transmissions');
 
-  const $msgBtn = $('<button>').text('Listen for incoming transmissions')
+  $body.prepend($msgBtn, $tweetHolder);
 
-  $body.prepend($msgBtn)
+  let generateTweets = (currentUser) => {
 
-  let generateTweets = ()=> {
-    let $tweets = streams.home.map((tweet) => {
-      const twid = `${tweet.message}`
-      
-      //  needs to change to clickable element
-      // username needs it's own tag
-      const $user = $('<a></a>')
-      .text(`@${tweet.user}`)
-      .attr('href', '#')
-      .addClass('username')
-      
-      const $tweet = $('<p></p>');
 
-      $div.prepend($tweet.text(`${user} ${twid} ${moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a')}`))
+    // Map over streams.home to generate tweets
+    currentUser.map((tweet) => {
+      const $tweet = $('<p></p>'); // Create a paragraph element for each tweet
+
+      // Create a clickable username element
+      const $userName = $('<a></a>')
+        .text(`@${tweet.user}`) // Set the username text
+        .addClass('username') // Add class
+        .click(function(e) {
+          $body.html('')
+          generateTweets(`${tweet.user}`)
+        })
+
+
+      //the tweet message
+      const $twid = `${tweet.message}`;
+
+      // Append all elements to the tweet (username, message, and timestamp)
+      $tweet.append($userName) // Add the username
+            .append(`: ${$twid} `) // Add the message
+            .append($('<span></span>').text(moment(tweet.created_at).format('MMMM Do YYYY, h:mm:ss a'))); // Add the timestamp
+
+      // Prepend the tweet to the container
+      $tweetHolder.prepend($tweet);
     });
-  }
+  };
 
-  // create a function that creates new tweets
-  $msgBtn.click(function(){
-    $div.html('')
-    generateTweets()
-  })
+  // Listen for button clicks to generate new tweets
+  $msgBtn.click(function () {
+    $tweetHolder.html(''); // Clear previous tweets
+    generateTweets(); // Generate new tweets
+  });
+
+
+
+
+
+
 
 });
 
